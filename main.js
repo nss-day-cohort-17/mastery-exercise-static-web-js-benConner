a33300871ce1e3c07b138895bf2c0ef77b55664a
Ben Conner <ben.conner40@gmail.com>
2016-12-02 14:55:32 -0600

    enter key press and tree function working

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
-var button = document.getElementById("btn-tree-grow");
-var treeHeight = document.getElementById("tree-hgt");
-var treeCharacter = document.getElementById("tree-char");
 
-function makeTree(){
-    console.log('*')
-    console.log(treeHeight.value);
-    console.log(treeCharacter.value);
-    console.log('*')
+var button = document.getElementById("btn-tree-grow");
+
+var treeHeight = document.getElementById("tree-hgt");
+var treeChar = document.getElementById("tree-char");
+var row = treeHeight.value;
+var r = row +(row += 1)
+var treeObj = {
+    height: "",
+    character: ""
+};
+
+function tree(){
+    treeObj.height = treeHeight.value
+    treeObj.character+= treeChar.value
+    if(treeObj.height > 0){
+        console.log(treeObj.character)
+        console.log()
+        treeHeight.value --;
+        tree();
+
+    }else{
+        console.log("Here is your tree")
+        treeObj.height = treeHeight.value
+        treeObj.character = treeChar.value
+    }
+
 }
 
-button.addEventListener("click", makeTree);
+function enterKey(e){
+    if(e.key === "Enter"){
+        console.log('hey')
+        tree()
+    }
+}
+
+
+function inputKeyUp(e) {
+    e.which = e.which || e.keyCode;
+    if(e.which == 13) {
+        // submit
+    }
+}
+
+button.addEventListener("click", tree);
+treeHeight.addEventListener("keypress", enterKey)
+treeChar.addEventListener("keypress", enterKey)
