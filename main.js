53278edcbc33969cdd1cc7b46087239839c311ce
Ben Conner <ben.conner40@gmail.com>
2016-12-04 19:53:44 -0600

    added object parameter to function and input validation

--- main.js
+++ main.js
@@ -2,35 +2,45 @@
 var button = document.getElementById("btn-tree-grow");
 var treeHeight = document.getElementById("tree-hgt");
 var treeChar = document.getElementById("tree-char");
-var space = " "
-var treeObj = {
-    height: "",
+var treeSpecs = {
+    height: 0,
     character: "",
+    space: " ",
+    Counter: 1,
 };
 
-function tree(){
-    treeObj.height = treeHeight.value
-    treeObj.character+= treeChar.value
-
+function tree(treeObj){
     if(treeObj.height > 0){
-
-        console.log( space.repeat(treeHeight.value) + treeObj.character + space.repeat(treeHeight.value))
-        treeHeight.value--
-        treeObj.character+= treeChar.value
-        tree();
+        console.log( treeObj.space.repeat(treeObj.height) + treeObj.character.repeat(treeObj.Counter) + treeObj.space.repeat(treeObj.height))
+        treeObj.height--
+        treeObj.Counter += 2;
+        tree(treeObj);
 
     }else {
         console.log("Here is your tree")
+        treeObj.Counter = 1;
+        return treeObj;
+
     }
+
+    // for (var i = treeHeight.value, j = 1; i > 0 ; i--, j+= 2) {
+    //     console.log( treeObj.space.repeat(i) + treeObj.character.repeat(j) + treeObj.space.repeat(i))
+    // }
 
 }
 // function for enter key-press
-function enterKey(e){
-    if(e.key === "Enter"){
-        tree()
+function submitting(e){
+    if((e.key === "Enter" || e.type === "click") && (treeHeight.value != "" || treeChar.value != "")){
+        if(treeHeight.value != "" && treeChar.value != ""){
+            treeSpecs.height = treeHeight.value
+            treeSpecs.character = treeChar.value
+            tree(treeSpecs)
+        }else {
+            alert("Please fill out both input fields");
+        }
     }
 }
 
-button.addEventListener("click", tree);
-treeHeight.addEventListener("keypress", enterKey)
-treeChar.addEventListener("keypress", enterKey)
+button.addEventListener("click", submitting);
+treeHeight.addEventListener("keypress", submitting);
+treeChar.addEventListener("keypress", submitting);
